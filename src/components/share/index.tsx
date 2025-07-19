5f9de79d7dd902304ca978fbaab0d7db98fab81c
Fairfarren <lykwowlol@gmail.com>
2025-07-19 18:43:09 +0800

    feat: share

---
+++ src/components/share/index.tsx
@@ -0,0 +1,42 @@
+import PATH_URL from '@/const/path'
+import { Share as AShare, type ShareProps } from '@taro-react-tools/components'
+import { View } from '@tarojs/components'
+import classnames from 'classnames'
+import { memo } from 'react'
+
+export const shareOption = {
+    title: '我在用小熊配货宝，自动打印配货单，好用！点击领积分！',
+    imageUrl:
+        'https://remember-quick.oss-cn-chengdu.aliyuncs.com/bear/share.png',
+    path: PATH_URL.INDEX,
+}
+/**
+ * 分享
+ * @param {string?} className
+ */
+export const Share = memo((props: ShareProps) => {
+    async function buttonShareBefore(e) {
+        if (e.target.dataset?.type === 'shareCard') {
+            return {
+                ...shareOption,
+            }
+        }
+        return props.promise?.(e)
+    }
+
+    return (
+        <>
+            <AShare
+                {...props}
+                promise={buttonShareBefore}
+                shareOption={{ ...shareOption, ...props.shareOption }}
+            >
+                {props.children}
+            </AShare>
+
+            <View
+                className={classnames('fixed', 'top-0', 'left-0', 'z-[999999]')}
+            ></View>
+        </>
+    )
+})
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,17 +1,22 @@
+import { Share } from '@/components/share'
 import { Button, View } from '@tarojs/components'
 import classnames from 'classnames'
 
 const Index = () => {
     return (
-        <View className={classnames('w-screen', 'h-screen')}>
-            <Button className="btn btn-active">Default</Button>
-            <Button className="btn btn-active btn-neutral">Neutral</Button>
-            <Button className="btn btn-active btn-primary">Primary</Button>
-            <Button className="btn btn-active btn-secondary">Secondary</Button>
-            <Button className="btn btn-active btn-accent">Accent</Button>
-            <Button className="btn btn-active btn-ghost">Ghost</Button>
-            <Button className="btn btn-active btn-link">Link</Button>
-        </View>
+        <Share>
+            <View className={classnames('w-screen', 'h-screen')}>
+                <Button className="btn btn-active">Default</Button>
+                <Button className="btn btn-active btn-neutral">Neutral</Button>
+                <Button className="btn btn-active btn-primary">Primary</Button>
+                <Button className="btn btn-active btn-secondary">
+                    Secondary
+                </Button>
+                <Button className="btn btn-active btn-accent">Accent</Button>
+                <Button className="btn btn-active btn-ghost">Ghost</Button>
+                <Button className="btn btn-active btn-link">Link</Button>
+            </View>
+        </Share>
     )
 }
 
