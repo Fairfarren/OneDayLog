eb1e92e63ab87fcfb6a5429560e0d525633dbad2
Fairfarren <lykwowlol@gmail.com>
2025-07-19 21:07:01 +0800

    feat: 日历

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,30 +1,25 @@
+import Calendar from '@/components/calendar'
 import DiyHeader from '@/components/diyHeader'
 import { Share } from '@/components/share'
-import { Button, Text, View } from '@tarojs/components'
+import { Text, View } from '@tarojs/components'
 import classnames from 'classnames'
 
 const Index = () => {
     return (
         <Share>
             <DiyHeader>
-                <View className={classnames('text-base-content')}>
-                    <Text>记时薄</Text>
+                <View
+                    className={classnames(
+                        'text-base-content',
+                        'text-2xl',
+                        'font-bole',
+                    )}
+                >
+                    <Text>一日所记</Text>
                 </View>
             </DiyHeader>
-            <View>
-                <Button className="btn btn-active">Default</Button>
-                <Button className="btn btn-active btn-neutral">Neutral</Button>
-                <Button className="btn btn-active btn-primary">Primary</Button>
-                <Button className="btn btn-active btn-secondary">
-                    Secondary
-                </Button>
-                <Button className="btn btn-active btn-accent">Accent</Button>
-                <Button className="btn btn-active btn-ghost">Ghost</Button>
-                <Button className="btn btn-active btn-error">btn-error</Button>
-                <Button className="btn btn-active btn-warning">
-                    btn-warning
-                </Button>
-                <Button className="btn btn-active btn-link">Link</Button>
+            <View className={classnames('container')}>
+                <Calendar />
             </View>
         </Share>
     )
