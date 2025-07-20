85d0ba2231cd9426eb3ac2fd44a68433e33e3bcb
Fairfarren <lykwowlol@gmail.com>
2025-07-21 00:13:09 +0800

    feat: swiper日历

--- src/components/calendar/daysCom.tsx
+++ src/components/calendar/daysCom.tsx
@@ -1,24 +1,38 @@
 import { useTime } from '@/store/time'
 import { View } from '@tarojs/components'
 import classnames from 'classnames'
+import dayjs from 'dayjs'
 import { useMemo } from 'react'
 
-function DaysCom() {
+function DaysCom(props: { month: number }) {
     const time = useTime()
 
     const getDoubleDay = useMemo(() => {
         return time.choiceDay === time.today ? time.today : false
     }, [time])
 
+    const list = useMemo(() => {
+        const days = dayjs(`${time.year}-${props.month}`)
+        const month = Array(days.daysInMonth())
+            .fill(null)
+            .map((_, i) => i + 1)
+        const firstDayOfMonth = days.startOf('month').day()
+
+        return {
+            firstDayOfMonth,
+            month,
+        }
+    }, [props.month, time])
+
     return (
         <View className={classnames('mt-2', 'grid', 'grid-cols-7', 'gap-2')}>
-            {Array(time.firstDayOfMonth)
+            {Array(list.firstDayOfMonth)
                 .fill(null)
                 .map((_day, i) => (
                     <View key={i} />
                 ))}
-            {time.days.map((day) => {
-                const accountDay = `${time.year}-${time.month}-${day}`
+            {list.month.map((day) => {
+                const accountDay = `${time.year}-${props.month}-${day}`
                 return (
                     <View
                         key={day}
@@ -26,7 +40,6 @@ function DaysCom() {
                             'kbd',
                             'kbd-md',
                             'bg-base-100',
-                            'transition',
                             getDoubleDay === accountDay
                                 ? classnames('rounded-full', 'bg-secondary')
                                 : classnames(
--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,17 +1,82 @@
 import DaysCom from '@/components/calendar/daysCom'
 import TimeTitle from '@/components/calendar/timeTitle'
-import { View } from '@tarojs/components'
+import { useTime } from '@/store/time'
+// import { Swiper } from '@nutui/nutui-react-taro'
+import { Swiper, SwiperItem, View } from '@tarojs/components'
 import classnames from 'classnames'
+import { useState } from 'react'
 import Week from './week'
 
 function Calendar() {
+    const time = useTime()
+    const [config, setConfig] = useState({
+        duration: 500,
+        current: 1,
+    })
+
+    function changeTime(e) {
+        console.log('onChange')
+        setConfig((data) => ({
+            ...data,
+            current: e.detail.current,
+        }))
+    }
+
+    function onFinish(e) {
+        let month = time.month
+        let year = time.year
+        setConfig((data) => ({
+            ...data,
+            duration: 0,
+        }))
+        setTimeout(() => {
+            if (e.detail.current < 1) {
+                month = month - 1
+            } else if (e.detail.current > 1) {
+                month = month + 1
+            }
+            if (month < 1) {
+                month = 12
+                year = year - 1
+            } else if (month > 12) {
+                month = 1
+                year = year + 1
+            }
+            time.update({
+                year,
+                month,
+            })
+            setConfig({
+                duration: 500,
+                current: 1,
+            })
+        }, 0)
+    }
+
     return (
         <View className={classnames('box', 'bg', 'text-center')}>
             <TimeTitle />
 
             <Week />
 
-            <DaysCom />
+            <Swiper
+                {...config}
+                style={{
+                    height: '430rpx',
+                }}
+                onChange={changeTime}
+                onAnimationFinish={onFinish}
+            >
+                <SwiperItem>
+                    <DaysCom month={time.month - 1} />
+                </SwiperItem>
+                <SwiperItem>
+                    <DaysCom month={time.month} />
+                </SwiperItem>
+                <SwiperItem>
+                    <DaysCom month={time.month + 1} />
+                </SwiperItem>
+            </Swiper>
         </View>
     )
 }
